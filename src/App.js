96cbaee5715c514f98242d4bcd02e73d02f4fa08
Adam M Duff <fikosan@gmail.com>
2018-09-26 08:45:26 -0400

    feat: Teams now loaded via nhl-stats api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    teams: [],
+    error: false
+  }
+  componentDidMount = () => {
+    this.pullTeams();
+  }
+
+  pullTeams = () => {
+    fetch('https://statsapi.web.nhl.com/api/v1/teams')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          teams: data.teams
+        })
+      })
+      .catch(err => console.log(err));
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }

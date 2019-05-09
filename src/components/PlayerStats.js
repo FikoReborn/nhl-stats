aeee2fe128c52c4d9b33fd8dd81fc579f711d0da
Adam M Duff <fikosan@gmail.com>
2019-05-09 09:36:31 -0400

    feat: Added stats for players

--- src/components/PlayerStats.js
+++ src/components/PlayerStats.js
@@ -12,7 +12,7 @@ class PlayerStats extends Component {
         return (
             <div className="player-info" key={player.id}>
                 {player.id && (
-                    <div>
+                    <div className="player-inner">
                         <div className="info">
                             <h3>{player.fullName}</h3>
                             <ul>
@@ -22,9 +22,141 @@ class PlayerStats extends Component {
                                 <li>{player.weight} lbs</li>
                             </ul>
                         </div>
-                        <div className="player-stats">
-test
+                        {player.primaryPosition.code === 'G' ? (
+                            <div className="player-stats">
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>Games Played</td>
+                                            <td>{playerStats.games}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Games Started</td>
+                                            <td>{playerStats.gamesStarted}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Wins</td>
+                                            <td>{playerStats.wins}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Losses</td>
+                                            <td>{playerStats.losses}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Overtime Losses:</td>
+                                            <td>{playerStats.ot}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Shots Against</td>
+                                            <td>{playerStats.shotsAgainst}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Goals Against</td>
+                                            <td>{playerStats.goalsAgainst}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>GAA</td>
+                                            <td>{playerStats.goalAgainstAverage}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Save %</td>
+                                            <td>{playerStats.savePercentage}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Shutouts</td>
+                                            <td>{playerStats.shutouts}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Minutes Played</td>
+                                            <td>{playerStats.timeOnIce}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                         </div>
+                        ) : (
+                        <div className="player-stats">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>Games Played</td>
+                                        <td>{playerStats.games}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Goals</td>
+                                        <td>{playerStats.goals}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Assists</td>
+                                        <td>{playerStats.assists}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Points</td>
+                                        <td>{playerStats.points}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>+/-</td>
+                                        <td>{playerStats.plusMinus}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>PIM</td>
+                                        <td>{playerStats.pim}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>PowerPlay Goals</td>
+                                        <td>{playerStats.powerPlayGoals}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>PowerPlay Points</td>
+                                        <td>{playerStats.powerPlayPoints}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Short-handed Goals</td>
+                                        <td>{playerStats.shortHandedGoals}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Short-handed Points</td>
+                                        <td>{playerStats.shortHandedPoints}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Game Winning Goals</td>
+                                        <td>{playerStats.gameWinningGoals}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Overtime Goals</td>
+                                        <td>{playerStats.overTimeGoals}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Shots</td>
+                                        <td>{playerStats.shots}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Shot %</td>
+                                        <td>{playerStats.shotPct}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Faceoff %</td>
+                                        <td>{playerStats.faceOffPct}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Average TOI</td>
+                                        <td>{playerStats.timeOnIcePerGame}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Even-strength TOI</td>
+                                        <td>{playerStats.evenTimeOnIcePerGame}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Short-handed TOI</td>
+                                        <td>{playerStats.shortHandedTimeOnIcePerGame}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>PowerPlay TOI</td>
+                                        <td>{playerStats.powerPlayTimeOnIcePerGame}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        )}
                     </div>
                 )}
 

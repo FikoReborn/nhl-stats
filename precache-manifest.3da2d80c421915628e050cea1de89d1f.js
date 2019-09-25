36aea62b7edca32468da3c455164e7f1c80ba7de
Adam M Duff <fikosan@gmail.com>
2019-09-25 14:17:46 -0400

    Updates

---
+++ precache-manifest.3da2d80c421915628e050cea1de89d1f.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2012b33ff42a7cdd248ae1e01147e59",
+    "url": "/nhl-stats/index.html"
+  },
+  {
+    "revision": "ffa57b0a422823e71d43",
+    "url": "/nhl-stats/static/css/main.35a12812.chunk.css"
+  },
+  {
+    "revision": "55388786928daac36ede",
+    "url": "/nhl-stats/static/js/2.dcf8c82d.chunk.js"
+  },
+  {
+    "revision": "ffa57b0a422823e71d43",
+    "url": "/nhl-stats/static/js/main.4486e0be.chunk.js"
+  },
+  {
+    "revision": "88a7c26e1c430ed22be6",
+    "url": "/nhl-stats/static/js/runtime~main.d4b2a7e6.js"
+  },
+  {
+    "revision": "9bc562de381d21c358f19d63d4745d24",
+    "url": "/nhl-stats/static/media/ANA.9bc562de.svg"
+  },
+  {
+    "revision": "8f690ed8d86668276ed7fd31d8cf01ab",
+    "url": "/nhl-stats/static/media/ARI.8f690ed8.svg"
+  },
+  {
+    "revision": "68807e3d8659e027fbb17533bf763457",
+    "url": "/nhl-stats/static/media/BOS.68807e3d.svg"
+  },
+  {
+    "revision": "34bd4af726ed4ee771183fc535ecbe09",
+    "url": "/nhl-stats/static/media/BUF.34bd4af7.svg"
+  },
+  {
+    "revision": "ca9445d273df8d642f1f4ee10de0b617",
+    "url": "/nhl-stats/static/media/CAR.ca9445d2.svg"
+  },
+  {
+    "revision": "616a5939c470976412e828328d8e5471",
+    "url": "/nhl-stats/static/media/CBJ.616a5939.svg"
+  },
+  {
+    "revision": "c86950f5738a40c0c6624b7567ac56b3",
+    "url": "/nhl-stats/static/media/CGY.c86950f5.svg"
+  },
+  {
+    "revision": "957756b02e648af9c155949a1f25ff85",
+    "url": "/nhl-stats/static/media/CHI.957756b0.svg"
+  },
+  {
+    "revision": "494be10fccbb94be7b7108e7448913ea",
+    "url": "/nhl-stats/static/media/COL.494be10f.svg"
+  },
+  {
+    "revision": "cfdb9c454375365cf3a0c43af9eb5f84",
+    "url": "/nhl-stats/static/media/DAL.cfdb9c45.svg"
+  },
+  {
+    "revision": "efabe2d1a75ddda9e5692857ca174772",
+    "url": "/nhl-stats/static/media/DET.efabe2d1.svg"
+  },
+  {
+    "revision": "a06389fd223656ac7e1803eb4f3dbe16",
+    "url": "/nhl-stats/static/media/EDM.a06389fd.svg"
+  },
+  {
+    "revision": "2cf605d9c6a29cb4a94fbe37106e7d78",
+    "url": "/nhl-stats/static/media/FLA.2cf605d9.svg"
+  },
+  {
+    "revision": "52edb823e06a107aa7e4281d75974f4d",
+    "url": "/nhl-stats/static/media/LAK.52edb823.svg"
+  },
+  {
+    "revision": "592cb8ab3588a262fa3262adf3eab20e",
+    "url": "/nhl-stats/static/media/MIN.592cb8ab.svg"
+  },
+  {
+    "revision": "86f58a6af903500f59bdbd235e48125a",
+    "url": "/nhl-stats/static/media/MTL.86f58a6a.svg"
+  },
+  {
+    "revision": "9a2e77167463e68e150317c88ea06f84",
+    "url": "/nhl-stats/static/media/NJD.9a2e7716.svg"
+  },
+  {
+    "revision": "3aa53cb343a2687872f0226c846cfc27",
+    "url": "/nhl-stats/static/media/NSH.3aa53cb3.svg"
+  },
+  {
+    "revision": "f384bd2c4e11cfe48dab997e5081d105",
+    "url": "/nhl-stats/static/media/NYI.f384bd2c.svg"
+  },
+  {
+    "revision": "784721fc45801982c61d383cd4bc9c36",
+    "url": "/nhl-stats/static/media/NYR.784721fc.svg"
+  },
+  {
+    "revision": "c8eee8095483d0874342816a2618682f",
+    "url": "/nhl-stats/static/media/OTT.c8eee809.svg"
+  },
+  {
+    "revision": "3c22873a82fb5c581fb9219d19d1c510",
+    "url": "/nhl-stats/static/media/PHI.3c22873a.svg"
+  },
+  {
+    "revision": "1efe47ecf7d79f43d8604979bf089da0",
+    "url": "/nhl-stats/static/media/PIT.1efe47ec.svg"
+  },
+  {
+    "revision": "1265334ca307c77de55c56968a0dd4be",
+    "url": "/nhl-stats/static/media/SJS.1265334c.svg"
+  },
+  {
+    "revision": "14ac936fb06d1730148dccff7ef5bfd5",
+    "url": "/nhl-stats/static/media/STL.14ac936f.svg"
+  },
+  {
+    "revision": "7f35b009223f704f088c1c5ee98e725e",
+    "url": "/nhl-stats/static/media/TBL.7f35b009.svg"
+  },
+  {
+    "revision": "20b99b8875908a6c96839a227cc3175b",
+    "url": "/nhl-stats/static/media/TOR.20b99b88.svg"
+  },
+  {
+    "revision": "be932ad30c5d2c3d505bc6e45b5e5206",
+    "url": "/nhl-stats/static/media/VAN.be932ad3.svg"
+  },
+  {
+    "revision": "08ecefc67493b9b79cb4ddb575ebe77b",
+    "url": "/nhl-stats/static/media/VGK.08ecefc6.svg"
+  },
+  {
+    "revision": "eae9751f8276b09c9103ba4bc1334491",
+    "url": "/nhl-stats/static/media/WPG.eae9751f.svg"
+  },
+  {
+    "revision": "95b22f4913c207856fa58a458b7c97b7",
+    "url": "/nhl-stats/static/media/WSH.95b22f49.svg"
+  }
+]);

c1ffb63720a648246de92cb4d5cecd760ee2c300
Adam M Duff <fikosan@gmail.com>
2019-09-25 13:38:19 -0400

    feat: Roster pulls out on mobile

--- src/components/TeamRoster.js
+++ src/components/TeamRoster.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import '../App.js';
 
 class TeamRoster extends Component {
+    state = {
+        width: this.props.windowWidth,
+        visible: false
+    }
+
+    handleRosterMenu = () => {
+        if (this.props.windowWidth <= 1060) {
+            this.setState({visible: !this.state.visible});
+        } 
+        
+    }
     render() {
+        const width = this.props.windowWidth;
         const roster = this.props.roster;
         let windowWidth = this.props.windowWidth;
+        let visible = this.state.visible;
         return (
-            <div className="team-stats">
+            <div className={visible ? 'rosterVisible' : 'rosterNotVisible'}>
                     <div className="stat-window">
-                    <div className="vertical">Roster</div>
+                    <div className="vertical" onClick={() => this.handleRosterMenu()}>Roster</div>
                         <table className="players-table">
                             <thead>
                                 <tr>

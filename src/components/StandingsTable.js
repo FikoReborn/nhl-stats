761c20557f3943fa967d4c07b83fe7cf439201dd
Adam M Duff <fikosan@gmail.com>
2019-09-26 21:24:39 -0400

    feat: removed unimportant columns from standings component

--- src/components/StandingsTable.js
+++ src/components/StandingsTable.js
@@ -9,7 +9,7 @@ class StandingsTable extends Component {
             <table className="standings-table">
                 <thead>
                     <tr>
-                        <th>{this.props.division}</th>
+                        <th className="div-name">{this.props.division}</th>
                         <th>GP</th>
                         <th>W</th>
                         <th>L</th>
@@ -19,10 +19,6 @@ class StandingsTable extends Component {
                         <th className="hide">GF</th>
                         <th className="hide">GA</th>
                         <th className="hide">DIFF</th>
-                        <th className="hide">Home</th>
-                        <th className="hide">Away</th>
-                        <th className="hide-small">S/O</th>
-                        <th className="hide">L10</th>
                         <th className="hide">Streak</th>
                     </tr>
                 </thead>
@@ -39,10 +35,6 @@ class StandingsTable extends Component {
                             <td className="hide">{currentTeam.goalsScored}</td>
                             <td className="hide">{currentTeam.goalsAgainst}</td>
                             <td className={(currentTeam.goalsScored - currentTeam.goalsAgainst) > 0 ? ("diff-positive hide") : ("diff-negative hide")}>{(currentTeam.goalsScored - currentTeam.goalsAgainst) > 0 && ("+")}{currentTeam.goalsScored - currentTeam.goalsAgainst}</td>
-                            <td className="hide">{currentTeam.records.overallRecords[0].wins} - {currentTeam.records.overallRecords[0].losses} - {currentTeam.records.overallRecords[0].ot}</td>
-                            <td className="hide">{currentTeam.records.overallRecords[1].wins} - {currentTeam.records.overallRecords[1].losses} - {currentTeam.records.overallRecords[1].ot}</td>
-                            <td className="hide-small">{currentTeam.records.overallRecords[2].wins} - {currentTeam.records.overallRecords[2].losses}</td>
-                            <td className="hide">{currentTeam.records.overallRecords[3].wins} - {currentTeam.records.overallRecords[3].losses} - {currentTeam.records.overallRecords[3].ot}</td>
                             <td className="hide">{currentTeam.streak.streakCode}</td>
                         </tr>
                     ))}
